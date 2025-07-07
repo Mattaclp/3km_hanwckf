eed362f899e5188493ebdb65739fe231be2c3f67
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2025-07-07 10:49:00 +0800

    Update diy-part2-oc101.sh

--- diy-part2-oc101.sh
+++ diy-part2-oc101.sh
@@ -15,6 +15,7 @@ cp -r helloworld package/
 git clone https://github.com/coolsnowwolf/packages.git
 cp -r packages/lang/rust feeds/packages/lang/
 rm -rf packages
+cp -r feeds/packages/lang/rust package/
 
 rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/luci/applications/luci-app-openclash
