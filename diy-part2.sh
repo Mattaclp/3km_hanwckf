478b98d8b39f68e7e590f15cce015c2be4161f8a
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2026-01-17 14:39:03 +0800

    Clean up gn version replacement from script
    Removed gn version replacement steps and related files.

--- diy-part2.sh
+++ diy-part2.sh
@@ -3,15 +3,6 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
 
-# replace gn version
-rm -rf feeds/packages/devel/gn/Makefile
-wget https://github.com/Mattaclp/NewLEDE/raw/refs/heads/main/gn/gnMakefile
-mv gnMakefile feeds/packages/devel/gn/Makefile
-rm -rf feeds/packages/devel/gn/src/out/last_commit_position.h
-wget https://github.com/Mattaclp/NewLEDE/raw/refs/heads/main/gn/src/out/last_commit_position.h
-mv last_commit_position.h feeds/packages/devel/gn/src/out/last_commit_position.h
-rm -rf feeds/packages/devel/gn/patches
-
 git clone https://github.com/xiaorouji/openwrt-passwall.git
 cp -r openwrt-passwall/luci-app-passwall package/
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git
